c4c3203c73503ea586b46b48c4c186cbf928681f
firebat-hacs <76677447+firebat-hacs@users.noreply.github.com>
2024-09-02 17:57:14 -0600

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -3,7 +3,7 @@
 #include "defaults.h"
 #include "hellen_leds_144.cpp"
 
-
+/*
 static void setInjectorPins() {
 	engineConfiguration->injectionPins[0] = H144_INJ1;
 	engineConfiguration->injectionPins[1] = H144_INJ2;
@@ -12,7 +12,8 @@ static void setInjectorPins() {
 	engineConfiguration->injectionPins[4] = H144_INJ5;
 	engineConfiguration->injectionPins[5] = H144_INJ6;
 }
-
+*/
+/*
 static void setIgnitionPins() {
 	engineConfiguration->ignitionPins[0] = H144_IGN7;
 	engineConfiguration->ignitionPins[1] = Gpio::Unassigned;
@@ -21,7 +22,8 @@ static void setIgnitionPins() {
 	engineConfiguration->ignitionPins[4] = Gpio::Unassigned;
 	engineConfiguration->ignitionPins[5] = Gpio::Unassigned;
 }
-
+*/
+/*
 static void setupDefaultSensorInputs() {
 	engineConfiguration->tps1_1AdcChannel = H144_IN_TPS_ANALOG;
 	engineConfiguration->tps1_2AdcChannel = H144_IN_AUX1_ANALOG;
@@ -50,16 +52,17 @@ static void setupDefaultSensorInputs() {
 	
 	
 }
-
+*/
 void setBoardConfigOverrides() {
 	setHellenMegaEnPin();
 	setHellenVbatt();
+	//setupVbatt();
 	hellenMegaSdWithAccelerometer();
   	setHellenCan();
 	setDefaultHellenAtPullUps();
+	//setHellenSdCardSpi2(); Card is on SPI1
 
 }
-
 
 void setBoardDefaultConfiguration() {
 	setInjectorPins();
@@ -69,12 +72,18 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->displayLogicLevelsInEngineSniffer = true;
 	engineConfiguration->globalTriggerAngleOffset = 9;
 	engineConfiguration->enableSoftwareKnock = true;
+	engineConfiguration->cylindersCount = 6;
+	engineConfiguration->firingOrder = FO_1_4_2_5_3_6;
+	engineConfiguration->ignitionMode = IM_SINGLE_POINT;
 	//engineConfiguration->canTxPin = Gpio::H144_CAN_TX;
 	//engineConfiguration->canRxPin = Gpio::H144_CAN_RX;
-	//engineConfiguration->mainRelayPin = Gpio::H144_IGN7;
- 	engineConfiguration->fanPin = Gpio::H144_OUT_IO13;
-	engineConfiguration->fuelPumpPin = Gpio::H144_OUT_IO8;
-	engineConfiguration->idle.solenoidPin = Gpio::H144_OUT_IO7;
+	//engineConfiguration->mainRelayPin = Gpio::H144_IGN7; //E10
+ 	//engineConfiguration->fanPin = Gpio::H144_OUT_IO13; //G6
+	//engineConfiguration->fuelPumpPin = Gpio::H144_OUT_IO8; //G4
+	//engineConfiguration->idle.solenoidPin = Gpio::H144_OUT_IO7; //G3
+	//engineConfiguration->tachOutputPin = Gpio::F13;
+	//engineConfiguration->clutchDownPin = Gpio::F5; // Clutch switch input
+	//engineConfiguration->clutchDownPinMode = PI_PULLDOWN;
 
 	// "required" hardware is done - set some reasonable defaults
 	setupDefaultSensorInputs();
@@ -88,65 +97,3 @@ void setBoardDefaultConfiguration() {
     	setTPS1Calibration(75, 900);
 	hellenWbo();
 }
-
-static Gpio OUTPUTS[] = {
-	Gpio::H144_INJ6, // B1 injector output 6
-	Gpio::H144_INJ5, // B2 injector output 5
-	Gpio::H144_INJ4, // B3 injector output 4
-	Gpio::H144_INJ3, // B4 injector output 3
-	Gpio::H144_INJ2, // B5 injector output 2
-	Gpio::H144_INJ1, // B6 injector output 1
-	Gpio::H144_INJ7, // B7 Low Side output 1
-	Gpio::H144_IGN8, // B8 Fan Relay Weak Low Side output 2
-	Gpio::H144_IGN7, // B9 Main Relay Weak Low Side output 1
-	Gpio::H144_OUT_PWM2, // B16 Low Side output 4 / Fuel Pump
-	Gpio::H144_OUT_PWM1, // B17 Low Side output 3
-	Gpio::H144_INJ8, // B18 Low Side output 2
-	// high sides
-	Gpio::H144_IGN6, // B10 Coil 6
-	Gpio::H144_IGN4, // B11 Coil 4
-	Gpio::H144_IGN3, // B12 Coil 3
-	Gpio::H144_IGN5, // B13 Coil 5
-	Gpio::H144_IGN2, // B14 Coil 2
-	Gpio::H144_IGN1, // B15 Coil 1
-};
-
-
-/*
-//old stuff
-
-
-
-
-// trigger inputs, hall
-engineConfiguration->triggerInputPins[0] = H144_IN_CRANK;
-engineConfiguration->camInputs[0] = H144_IN_CAM;
-//sensors
-engineConfiguration->tps1_1AdcChannel = H144_IN_TPS;
-engineConfiguration->mafAdcChannel = EFI_ADC_10;
-engineConfiguration->afr.hwChannel = EFI_ADC_1;
-engineConfiguration->clt.adcChannel = H144_IN_CLT;
-engineConfiguration->iat.adcChannel = H144_IN_IAT;
-//outputs
-engineConfiguration->fuelPumpPin = Gpio::G4;	// OUT_IO8
-engineConfiguration->idle.solenoidPin = Gpio::G3;	// OUT_IO7
-engineConfiguration->fanPin = Gpio::G6;	// OUT_IO13
-engineConfiguration->mainRelayPin = Gpio::E10;
-engineConfiguration->tachOutputPin = Gpio::F13;
-//inputs
-engineConfiguration->clutchDownPin = Gpio::F5; // Clutch switch input
-engineConfiguration->clutchDownPinMode = PI_PULLDOWN;
-engineConfiguration->launchActivationMode = CLUTCH_INPUT_LAUNCH;
-engineConfiguration->malfunctionIndicatorPin = Gpio::Unassigned;
-//engine parameters
-engineConfiguration->cylindersCount = 6;
-engineConfiguration->firingOrder = FO_1_4_2_5_3_6;
-engineConfiguration->ignitionMode = IM_SINGLE_POINT;
-
-void setBoardConfigOverrides() {
-	setupVbatt();
-	setHellenSdCardSpi2();
-	setDefaultHellenAtPullUps();
-	setHellenCan();
-}
-*/
