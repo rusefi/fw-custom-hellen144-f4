c3538e2797defc3a542de4ba577e7840281a5a93
Andrey <rusefillc@gmail.com>
2024-09-02 09:54:53 -0400

    Bluetooth?

--- board.mk
+++ board.mk
@@ -10,4 +10,8 @@ include $(BOARD_DIR)/meta-info.env
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
+# this is usually used by Bluetooth
+DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
+
+# this includes knock and mux control settings
 include $(BOARDS_DIR)/hellen/hellen-common-mega144.mk
