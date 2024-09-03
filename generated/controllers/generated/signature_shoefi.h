fe3acd1751e9add8eea9977df12010e090364340
GitHub git update Action <action@github.com>
2024-09-03 16:03:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_shoefi.h
+++ generated/controllers/generated/signature_shoefi.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 43759232
-#define TS_SIGNATURE "rusEFI main.2024.09.03.shoefi.43759232"
+#define SIGNATURE_HASH 3174773160
+#define TS_SIGNATURE "rusEFI main.2024.09.03.shoefi.3174773160"
