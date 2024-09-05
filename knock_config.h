4f1d4b4ea7fc4327f34cbe44bf882b4370ac02ff
firebat-hacs <76677447+firebat-hacs@users.noreply.github.com>
2024-09-04 18:39:39 -0600

    Update knock_config.h

--- knock_config.h
+++ knock_config.h
@@ -1,5 +1,5 @@
 /**
- * @file knock_config.h
+ * @file hellen_knock_config.h
  */
 
 #pragma once
@@ -7,9 +7,9 @@
 // Knock is on ADC3
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 Gpio::A3
+// knock 1 - pin PF7
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN5
+#define KNOCK_PIN_CH1 Gpio::F7
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
