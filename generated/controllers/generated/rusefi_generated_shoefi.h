704829e9625030ffbd947488eac8cc3b6dcf9c53
GitHub git update Action <action@github.com>
2024-09-05 00:53:49 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_shoefi.h
+++ generated/controllers/generated/rusefi_generated_shoefi.h
@@ -1297,7 +1297,7 @@
 #define show_Proteus_presets false
 #define show_test_presets false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1966288201
+#define SIGNATURE_HASH 2521290016
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1692,7 +1692,7 @@
 #define ts_show_oil_pressure_sensor true
 #define ts_show_oil_temp_sensor true
 #define ts_show_onboard_accelerometer true
-#define ts_show_output_diag false
+#define ts_show_output_diag true
 #define ts_show_popular_vehicles true
 #define ts_show_reboot_to_dfu true
 #define ts_show_reset_calibrations false
@@ -1743,7 +1743,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.09.05.shoefi.1966288201"
+#define TS_SIGNATURE "rusEFI main.2024.09.05.shoefi.2521290016"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_shoefi.h
+++ generated/controllers/generated/signature_shoefi.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1966288201
-#define TS_SIGNATURE "rusEFI main.2024.09.05.shoefi.1966288201"
+#define SIGNATURE_HASH 2521290016
+#define TS_SIGNATURE "rusEFI main.2024.09.05.shoefi.2521290016"
