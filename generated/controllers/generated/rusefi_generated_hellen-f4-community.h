ecb46c48777ba4b89be025a17bfc9fe0cea6eb0b
GitHub git update Action <action@github.com>
2024-10-20 16:45:18 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1760,7 +1760,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.10.19.hellen-f4-community.3790488119"
+#define TS_SIGNATURE "rusEFI main.2024.10.20.hellen-f4-community.3790488119"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
