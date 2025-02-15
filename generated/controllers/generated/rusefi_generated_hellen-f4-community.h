78b9676ac9cbf77b841f8a4a095641680177b376
GitHub git update Action <action@github.com>
2025-02-15 16:45:23 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1380,7 +1380,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 908321438
+#define SIGNATURE_HASH 824168676
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1814,6 +1814,7 @@
 #define ts_show_spi1_pins true
 #define ts_show_spi2_enable true
 #define ts_show_spi2_pins true
+#define ts_show_spi3_enable true
 #define ts_show_spi3_pins true
 #define ts_show_spi4_enable true
 #define ts_show_spi4_pins false
@@ -1852,14 +1853,14 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.02.14.hellen-f4-community.908321438"
+#define TS_SIGNATURE "rusEFI main.2025.02.15.hellen-f4-community.824168676"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
 #define TS_TEST_COMMAND_char t
-#define TS_TOTAL_OUTPUT_SIZE 1812
+#define TS_TOTAL_OUTPUT_SIZE 1804
 #define TS_TRIGGER_SCOPE_CHANNEL_1_NAME "Channel 1"
 #define TS_TRIGGER_SCOPE_CHANNEL_2_NAME "Channel 2"
 #define TS_TRIGGER_SCOPE_DISABLE 5
--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 908321438
-#define TS_SIGNATURE "rusEFI main.2025.02.14.hellen-f4-community.908321438"
+#define SIGNATURE_HASH 824168676
+#define TS_SIGNATURE "rusEFI main.2025.02.15.hellen-f4-community.824168676"
