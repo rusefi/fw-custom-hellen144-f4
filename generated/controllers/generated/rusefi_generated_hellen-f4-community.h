2e4956a96e109c202dd3680f26b41923c1c5b777
GitHub git update Action <action@github.com>
2025-02-24 16:46:45 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1383,7 +1383,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1307076841
+#define SIGNATURE_HASH 3587565632
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1861,6 +1861,10 @@
 #define ts_show_useRunningMathForCranking true
 #define ts_show_useSeparateVeForIdle true
 #define ts_show_vbatt true
+#define ts_show_ve_blend true
+#define ts_show_veBlends2 true
+#define ts_show_veBlends3 true
+#define ts_show_veBlends4 true
 #define ts_show_vehicle_speed_sensor true
 #define ts_show_vr_threshold_2 false
 #define ts_show_vr_threshold_all false
@@ -1870,7 +1874,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.02.23.hellen-f4-community.1307076841"
+#define TS_SIGNATURE "rusEFI main.2025.02.24.hellen-f4-community.3587565632"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1307076841
-#define TS_SIGNATURE "rusEFI main.2025.02.23.hellen-f4-community.1307076841"
+#define SIGNATURE_HASH 3587565632
+#define TS_SIGNATURE "rusEFI main.2025.02.24.hellen-f4-community.3587565632"
