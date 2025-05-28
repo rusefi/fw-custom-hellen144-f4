b960a8fb0d935c83281d9a41a51e38a80ef5387e
GitHub git update Action <action@github.com>
2025-05-28 16:48:20 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2217145613
-#define TS_SIGNATURE "rusEFI main.2025.05.27.hellen-f4-community.2217145613"
+#define SIGNATURE_HASH 3349890445
+#define TS_SIGNATURE "rusEFI main.2025.05.28.hellen-f4-community.3349890445"
