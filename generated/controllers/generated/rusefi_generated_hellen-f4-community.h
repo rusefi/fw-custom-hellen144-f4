5a42be6408f5f444c6508169e264d49bb55ae64c
GitHub git update Action <action@github.com>
2025-10-24 16:49:23 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1539,7 +1539,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2044958457
+#define SIGNATURE_HASH 3333621470
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2097,6 +2097,7 @@
 #define ts_show_tsp_pps_limits true
 #define ts_show_tunerstudio_port true
 #define ts_show_twoWireBatchIgnition true
+#define ts_show_useNoiselessTriggerDecoder true
 #define ts_show_useRunningMathForCranking true
 #define ts_show_useSeparateVeForIdle true
 #define ts_show_vbatt true
@@ -2118,7 +2119,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.10.23.hellen-f4-community.2044958457"
+#define TS_SIGNATURE "rusEFI main.2025.10.24.hellen-f4-community.3333621470"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
@@ -2145,7 +2146,6 @@
 #define TsCalMode_Tps2Min 9
 #define TsCalMode_Tps2SecondaryMax 10
 #define TsCalMode_Tps2SecondaryMin 11
-#define TUNING_DETECTOR_PERSISTENCE_OFFSET -1
 #define uart_device_e_auto_enum 0="UART_NONE",1="UART_DEVICE_1",2="UART_DEVICE_2",3="UART_DEVICE_3",4="UART_DEVICE_4"
 #define uart_device_e_UART_DEVICE_1 1
 #define uart_device_e_UART_DEVICE_2 2
--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2044958457
-#define TS_SIGNATURE "rusEFI main.2025.10.23.hellen-f4-community.2044958457"
+#define SIGNATURE_HASH 3333621470
+#define TS_SIGNATURE "rusEFI main.2025.10.24.hellen-f4-community.3333621470"
