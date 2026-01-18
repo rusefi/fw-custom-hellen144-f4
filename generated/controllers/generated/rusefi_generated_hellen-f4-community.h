c75a1d355ca7bc49856cf6511d3fc596e242910c
GitHub git update Action <action@github.com>
2026-01-18 16:48:25 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1395,6 +1395,7 @@
 #define LUA_SCRIPT_SIZE 8000
 #define MAF_DECODING_COUNT 32
 #define MAIN_HELP_URL "http://www.rusefi.com/"
+#define MAIN_PAGE_GAUGES_FILE "tunerstudio/main_page_gauges.ini"
 #define MAP_ANGLE_SIZE 8
 #define MAP_EST_LOAD_COUNT 16
 #define MAP_EST_RPM_COUNT 16
@@ -1548,7 +1549,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3616345252
+#define SIGNATURE_HASH 3419752645
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1617,6 +1618,7 @@
 #define TOOTH_PACKET_COUNT 1000
 #define TOOTH_PACKET_SIZE 2
 #define TOP_DEAD_CENTER_MESSAGE "r"
+#define TOP_LEVEL_MENU_FILE "tunerstudio/top_level_menu.ini"
 #define TORQUE_CURVE_RPM_SIZE 6
 #define TORQUE_CURVE_SIZE 6
 #define TORQUE_TABLE_X_SIZE 6
@@ -2139,7 +2141,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2026.01.17.hellen-f4-community.3616345252"
+#define TS_SIGNATURE "rusEFI main.2026.01.18.hellen-f4-community.3419752645"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3616345252
-#define TS_SIGNATURE "rusEFI main.2026.01.17.hellen-f4-community.3616345252"
+#define SIGNATURE_HASH 3419752645
+#define TS_SIGNATURE "rusEFI main.2026.01.18.hellen-f4-community.3419752645"
