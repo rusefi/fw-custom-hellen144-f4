fdcedc1a1beafb62725b6be4eed6bf7c2b27d01a
GitHub git update Action <action@github.com>
2026-05-06 17:11:49 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -2179,7 +2179,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.05.05.hellen-f4-community.3988467484"
+#define TS_SIGNATURE "rusEFI main.2026.05.06.hellen-f4-community.3988467484"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
