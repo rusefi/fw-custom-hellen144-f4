6b16b1443fe8b9a117c9610dd5fc67101b8bba83
GitHub git update Action <action@github.com>
2026-07-22 17:06:44 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-f4-community.h
+++ generated/controllers/generated/rusefi_generated_hellen-f4-community.h
@@ -1630,7 +1630,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4246491304
+#define SIGNATURE_HASH 469382006
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2268,7 +2268,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.07.21.hellen-f4-community.4246491304"
+#define TS_SIGNATURE "rusEFI main.2026.07.22.hellen-f4-community.469382006"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_hellen-f4-community.h
+++ generated/controllers/generated/signature_hellen-f4-community.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4246491304
-#define TS_SIGNATURE "rusEFI main.2026.07.21.hellen-f4-community.4246491304"
+#define SIGNATURE_HASH 469382006
+#define TS_SIGNATURE "rusEFI main.2026.07.22.hellen-f4-community.469382006"
